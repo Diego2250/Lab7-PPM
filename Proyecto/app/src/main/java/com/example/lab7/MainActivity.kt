7b4fb39c55cc1ea4ada95015b2003ef055e688c5
Diego Morales <mor21146@uvg.edu.gt>
2022-08-29 01:01:07 -0600

    apk

---
+++ Proyecto/app/src/main/java/com/example/lab7/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.lab7
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import android.os.Bundle
+import androidx.fragment.app.commit
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+}
